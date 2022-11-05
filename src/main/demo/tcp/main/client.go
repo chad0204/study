18a21e911d0b41247c7cd64cc9ea051f85174cca
pengchao <pengchao@lianlianpay.com>
2022-11-05 16:23:56 +0800

    feat: demo

---
+++ src/main/demo/tcp/main/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	conn, err := net.Dial("tcp", "localhost:50000")
+	if err != nil {
+		fmt.Println("Error dialing", err.Error())
+		return
+	}
+
+	//接收键盘输入
+	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Println("First, what is your name?")
+	clientName, _ := inputReader.ReadString('\n')
+	trimmedClient := strings.Trim(clientName, "\r\n")
+
+	//发送数据到服务器
+	for {
+		fmt.Println("What to send to the server? Type Q to quit.")
+		input, _ := inputReader.ReadString('\n')
+		trimmedInput := strings.Trim(input, "\r\n")
+		if trimmedInput == "Q" {
+			return
+		}
+		_, err = conn.Write([]byte(trimmedClient + " says: " + trimmedInput))
+	}
+
+}
