660d487ca09d0f3609a2d404c4cf3d4c6ca8e3ee
pengchao <pengchao@lianlianpay.com>
2022-11-20 14:12:09 +0800

    feat: sync

--- src/main/effective/sync_test.go
+++ src/main/effective/sync_test.go
@@ -11,27 +11,33 @@ func Deposit(amount int) {
 	balance = balance + amount
 }
 
+func Balance() int {
+	return balance
+}
+
 func TestBankUnSafe(t *testing.T) {
 
 	countNum := 0 //执行次数
 	errNum := 0   //错误次数
 	for {
+		balance = 0 //每次开始需要重新置为0
 		countNum++
+
 		//wait保证两个goroutine都执行完成再进行判断
-		wait := make(chan int, 2)
+		done := make(chan int)
 		//g1
 		go func() {
 			Deposit(100)
-			wait <- 1
+			done <- 1
 		}()
 		//g2
 		go func() {
 			Deposit(200)
-			wait <- 1
+			done <- 1
 		}()
 
-		<-wait
-		<-wait
+		<-done
+		<-done
 		if balance != 300 {
 			/**
 			g1 read balance(0) + amount(100)
@@ -44,10 +50,55 @@ func TestBankUnSafe(t *testing.T) {
 
 			errNum++
 			fmt.Printf("balance = %v, count = %v; error = %v \n", balance, countNum, errNum)
-			balance = 0
-		}
-		if balance == 300 {
-			balance = 0
+
 		}
 	}
+}
+
+//线程安全改造
+
+var deposits = make(chan int) //发送存钱事件
+var balances = make(chan int) //金额
+
+func DepositSafe(amount int) {
+	deposits <- amount
+}
+
+func BalanceSafe() int {
+	return <-balances
+}
+
+func TestBankSafe(t *testing.T) {
+
+	//先启动读监听
+	go func() {
+		var b int
+		for {
+			select {
+			case amount := <-deposits:
+				b = b + amount
+			case balances <- b:
+
+			}
+		}
+	}()
+
+	done := make(chan int)
+
+	//g1
+	go func() {
+		DepositSafe(100)
+		done <- 0
+	}()
+	//g2
+	go func() {
+		DepositSafe(200)
+		done <- 0
+	}()
+
+	<-done
+	<-done
+
+	fmt.Println(BalanceSafe() == 300)
+
 }
